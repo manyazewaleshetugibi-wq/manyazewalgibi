cf5b93b951f31008cc77d2a4ee3b9cbae532d5dd
wedajiemisgan8@gmail.com <kkidusan>
2026-01-28 20:18:59 -0800

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Manyazewal Eshetu Gibi | Ethiopian & International Cuisine",
   description: "Experience a unique fusion of Ethiopian cultural dishes and international favorites at our culinary oasis in the heart of Bole. Enjoy organic ingredients and attentive service in our welcoming atmosphere.",
+  icons: {
+      icon: '/favicon.ico', // Example: spaghetti emoji
+ 
+  },
 };
 
 export default function RootLayout({
