84507b7dde440b55dac66114e2d4625afe22aab6
wedajiemisgan8@gmail.com <kkidusan>
2026-05-29 14:13:42 +0300

    config teail

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,7 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//   },
-// };
-
-// export default config;
-
-
-// postcss.config.mjs
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
   },
 };
 
