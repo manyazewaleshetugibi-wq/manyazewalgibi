18cd19b67719a44c680c42f326924f800752266c
kkidusan <wedajiemisgan8@gmail.com>
2026-06-23 02:58:39 -0700

    m

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -5,7 +5,6 @@ const MOBILE_BREAKPOINT = 768
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
-// kik
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
